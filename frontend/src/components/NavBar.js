9b7e439046eed47a7eac41ac852f7abcd5dc9f3d
AlessandroPozzoni <alessandro1.pozzoni@mail.polimi.it>
2021-12-17 13:43:04 +0000

    worked on frontend

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,28 @@
-//import { makeStyles } from '@mui/styles';
+import { makeStyles } from '@mui/styles';
 import reactDom from 'react-dom';
 import Section from './Section';
 
 
-// const useStyles = makeStyles({
-//     navBar: {
-//         background: 'red',
-//         height: 100,
-//         width: '100%'   
-//     },
-// })
+const useStyles = makeStyles({
+    navBar: {
+        background: 'red',
+        height: 100,
+        align: 'center',
+        width: '100%',
+        display: 'inline-flex',
+        justifyContent: 'space-evenly',
+    },
+})
 
 function NavBar() {
-    //const classes = useStyles()
+    const classes = useStyles()
 
 
     return(
-        <div>
+        <div className={classes.navBar}>
             <Section href='/' name='Home'/>
-            <Section href='ranking' name='Add candidate'/>
-            <Section href = 'add_candidate' name='View ranking'/>
+            <Section href='add_candidate' name='Add candidate'/>
+            <Section href = 'ranking' name='View ranking'/>
         </div>
     )
 }
