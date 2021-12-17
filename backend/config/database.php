9daba28ece5f75521695a126487dd4844844c249
Kiti Suupohja <kiti.suupohja@futurice.com>
2021-12-17 14:22:52 +0000

    Change db

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -15,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('CLEARDB_DATABASE_URL', 'heroku'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +41,16 @@ return [
     */
 
     'connections' => [
-
+        'heroku' => array(
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ),
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
