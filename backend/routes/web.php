9469fc0310011c340b4ed82e6a9e53589ff7b473
kursadcan <kursadcan.akay@ceng.deu.edu.tr>
2021-12-17 14:52:51 +0300

    Routes Blue Print Added

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('You Are Welcome To High Tech Satellite Of Trio\'s');
+
 });
+Route::get('/ranking', function () {
+    return view('Ranking Page');
+
+});
+Route::post('/add_candidate', function () {
+    return view('Add Candidate Page');
+});
+
+
