9db498a1acb672f23c638f0ae4866226079a63e7
kursadcan <kursadcan.akay@ceng.deu.edu.tr>
2021-12-17 17:22:41 +0300

    Recınf7

--- backend/config/database.php
+++ backend/config/database.php
@@ -34,7 +34,7 @@ return [
     */
 
     'connections' => [
-
+        /*
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
@@ -42,15 +42,15 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        */,
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql11.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'USERS'),
+            'username' => env('DB_USERNAME', 'sql11459641'),
+            'password' => env('DB_PASSWORD', 'WDPKImAlAE'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
+   /*,
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -78,6 +78,7 @@ return [
             'sslmode' => 'prefer',
         ],
 
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
@@ -90,6 +91,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+        */
 
     ],
 
