ac9b1cbafc36b761e856af7286e4d53cb6229ae1
AlessandroPozzoni <alessandro1.pozzoni@mail.polimi.it>
2021-12-17 13:54:01 +0000

    worked on frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,9 @@
 function Home(){
     return(
         <div>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/d/d2/EESTEC_Logo.png'/>
-            <h1 style={{color: 'red'}}>
-                Official Recruiting App
+            <img src='https://upload.wikimedia.org/wikipedia/commons/d/d2/EESTEC_Logo.png' style={{height: 450, width: 'auto'}}/>
+            <h1 style={{color: 'red', fontWeight: 'lighter'}}>
+                (Not so) Official Recruiting App
             </h1>
         </div>
     )
