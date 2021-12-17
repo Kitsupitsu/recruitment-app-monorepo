8e7e09b7b3a9815634ab79fab4dafdc5e7f9e0bc
AlessandroPozzoni <alessandro1.pozzoni@mail.polimi.it>
2021-12-17 13:48:30 +0000

    worked on frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/d/d2/EESTEC_Logo.png'/>
-            <h1>
+            <h1 style={{color: 'red'}}>
                 Official Recruiting App
             </h1>
         </div>
